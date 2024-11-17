553616b743c52708939e5d7d4dcdd9d0d39b1c72
Twicks <teguh.wck95@gmail.com>
2024-11-17 14:53:54 +0700

    Modify root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,9 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
+          <ul className="mb-2">
+            This will become the site for Twicksart&apos;s portfolio.
+          </ul>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
